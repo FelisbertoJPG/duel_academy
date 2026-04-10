339fe53ed7f4d2d2f09faa470b449943b75587d3
FelisbertoJPG <joaopfschmidt@gmail.com>
2026-04-10 00:26:26 -0300

    feat: setup de interações NPC, balões de diálogo e scripts utilitários

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInteraction : MonoBehaviour
-{
+{       
+    [SerializeField] private GameObject speechBalloon;
+    private bool interactionEnabled = false;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,30 @@ public class PlayerInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (interactionEnabled && Keyboard.current != null && Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            if (speechBalloon != null)
+            {
+                speechBalloon.SetActive(true);
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<InteractablePoint>(out InteractablePoint interactable))
+        {
+            interactionEnabled = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<InteractablePoint>(out InteractablePoint interactable))
+        {
+            interactionEnabled = false;
+            if (speechBalloon != null)
+            {
+                speechBalloon.SetActive(false);
+            }
+        }
     }
 }
